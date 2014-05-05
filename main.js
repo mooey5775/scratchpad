904ba0211480691936f72941779ba141b90cddf6
mooey5775 <edwardyaoli@gmail.com>
2014-05-05 19:26:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 // Hello.
 function viewJS() {
   var jstext = document.getElementById("jstext");
-  var urltext = "javascript:".concat(jstext.value);
-  var win = window.open(urltext, '_blank');
-  win.focus();
+  eval(jstext.value);
 }
