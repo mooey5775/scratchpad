593730cd514dd44bd71b3ddf8895c2c09cbba1d7
mooey5775 <edwardyaoli@gmail.com>
2014-05-05 19:31:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 // Hello.
 function viewJS() {
-  var jstext = document.getElementById("jstext");
-  eval(jstext.value);
+  var jstext = document.getElementById("jstext").value;
+  eval(jstext);
+}
+
+function saveJS() {
+  var jstext = document.getElementById("jstext").value;
+  document.cookie = "jstext=".concat(jstext);
 }
