f4a77c94119f876d656545c1f628b770eb9bbd88
mooey5775 <edwardyaoli@gmail.com>
2014-05-05 19:37:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,27 @@ function saveJS() {
   var jstext = document.getElementById("jstext").value;
   document.cookie = "jstext=".concat(jstext);
 }
+
+function loadJS() {
+  var jstext=getCookie("jstext");
+  if (jstext!="")
+  {
+    document.getElementById("jstext").value = jstext;
+  }
+  else 
+  {
+    alert("No save!");
+  }
+}
+
+function getCookie(cname)
+{
+var name = cname + "=";
+var ca = document.cookie.split(';');
+for(var i=0; i<ca.length; i++) 
+  {
+  var c = ca[i].trim();
+  if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+  }
+return "";
+}
