4343dbfecb73428914b7d07d7afd7639796a66bd
mooey5775 <edwardyaoli@gmail.com>
2014-05-06 21:12:07 -0400

    Update main.js

--- main.js
+++ main.js
@@ -56,18 +56,40 @@ function viewHTML() {
 }
 
 function saveHTML() {
-  var htmltext = document.getElementById("htmltext").value;
-  localStorage.setItem("htmltext", htmltext);
+  var jstext = document.getElementById("htmltext").value;
+  var name = prompt("Name of file", "Untitled");
+  if (name != "") {
+    if (localStorage.getItem("htmltext_".concat(name)) != null) {
+      var yes = confirm('Do you want to overwrite the existing file: '.concat(name).concat("?"));
+      if (yes == true) {
+        localStorage.setItem("htmltext_".concat(name), jstext);
+      }
+    } else {
+      localStorage.setItem("htmltext_".concat(name), jstext);
+      alert('Saved!');
+    }
+  } else {
+    alert('Please enter a name.');
+    saveJS();
+  }
 }
 
 function loadHTML() {
-  var htmltext=localStorage.getItem("htmltext");
-  if (htmltext === null)
+  var files = "";
+  for (var i = 0; i < localStorage.length; i++) {
+    var key = localStorage.key(i);
+    if (key.substr(0, 8) == "htmltext") {
+      files = files.concat(key.substr(9)).concat(" | ");
+    }
+  }
+  var name = prompt("Choose file: ".concat(files))
+  var jstext=localStorage.getItem("htmltext_".concat(name));
+  if (jstext === null)
   {
-    alert("No save!");
+    alert("No save found!");
   }
   else 
   {
-    document.getElementById("htmltext").value = htmltext;
+    document.getElementById("htmltext").value = jstext;
   }
 }
